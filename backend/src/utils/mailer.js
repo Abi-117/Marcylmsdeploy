007bdf8d40c8dfdb529dd052f42f511c26dd4c09
Abinaya <abinaya711s@gmail.com>
2026-05-29 17:25:03 +0530

    fix payment verify

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -3,25 +3,31 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // IMPORTANT
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
   tls: {
     rejectUnauthorized: false,
+    minVersion: "TLSv1.2",
   },
 });
 
 export const sendOTPEmail = async (email, otp) => {
-  return await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "Your OTP Code",
-    html: `
-      <h2>Password Reset OTP</h2>
-      <h1>${otp}</h1>
-      <p>Valid for 5 minutes</p>
-    `,
-  });
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "OTP Verification",
+      html: `<h1>${otp}</h1>`,
+    });
+
+    console.log("MAIL SENT:", info.response);
+    return info;
+
+  } catch (err) {
+    console.log("MAIL ERROR FULL:", err);
+    throw err;
+  }
 };
