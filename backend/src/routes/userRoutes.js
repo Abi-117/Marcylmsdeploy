6a4e846e7c6ca9f21728a0b0f2f44b99598ff270
Abinaya <abinaya711s@gmail.com>
2026-05-30 23:28:37 +0530

    update

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
-
 import User from "../models/User.js";
+import Course from "../models/Course.js";
 import jwt from "jsonwebtoken";
 import multer from "multer";
 import path from "path";
