dd2d8d3b346807a7dd547f39464b28137ee91d77
Abinaya <abinaya711s@gmail.com>
2026-06-03 13:27:05 +0530

    design

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,5 +1,12 @@
 import nodemailer from "nodemailer";
 
+
+console.log("EMAIL_USER =", process.env.EMAIL_USER);
+
+console.log(
+  "EMAIL_PASS =",
+  process.env.EMAIL_PASS ? "FOUND" : "NOT FOUND"
+);
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
